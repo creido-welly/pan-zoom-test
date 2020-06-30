149a49938a298e212c5636d05e7b9fcba84d75a9
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-06-30 11:58:21 +0100

    Updates

---
+++ precache-manifest.e1696531e40ef56f5ff24fa3ad718e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917becb640f2fbb536e06b7b0dad26ad",
+    "url": "/pan-zoom-test/index.html"
+  },
+  {
+    "revision": "62acb31c52e9913ada3b",
+    "url": "/pan-zoom-test/static/css/main.b549f582.chunk.css"
+  },
+  {
+    "revision": "a80c9698f57afbf3de3e",
+    "url": "/pan-zoom-test/static/js/2.1969f418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pan-zoom-test/static/js/2.1969f418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62acb31c52e9913ada3b",
+    "url": "/pan-zoom-test/static/js/main.3cbede24.chunk.js"
+  },
+  {
+    "revision": "fbd58efaf259a91e6b53",
+    "url": "/pan-zoom-test/static/js/runtime-main.f7320839.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pan-zoom-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
